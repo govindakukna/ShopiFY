f6c2c00d382964eb4f6237eb9907c49df3d26e4a
govindakukna <kuknagovinda@gmail.com>
2024-07-07 14:29:30 +0530

    login , signup , productdetails and cart page added succesfully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import Checkout from "./pages/Checkuot";
 
 import {
   createBrowserRouter,
@@ -11,13 +10,23 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import Cart from "./features/cart/Cart";
 import CartPage from "./pages/CartPage";
-import ProductDetailPage from "./pages/ProductDetalPage";
-
+import Checkout from "./pages/Checkout";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import Protected from "./features/auth/components/Protected";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectLoggedInUser } from "./features/auth/authSlice";
+import { fetchItemsByUserIdAsync } from "./features/cart/cartSlice";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    element: (
+      <Protected>
+        <Home></Home>
+      </Protected>
+    ),
   },
   {
     path: "/login",
@@ -29,19 +38,40 @@ const router = createBrowserRouter([
   },
   {
     path: "/cart",
-    element: <CartPage></CartPage>,
+    element: (
+      <Protected>
+        <CartPage></CartPage>
+      </Protected>
+    ),
   },
   {
     path: "/checkout",
-    element: <Checkout></Checkout>,
+    element: (
+      <Protected>
+        <Checkout></Checkout>
+      </Protected>
+    ),
   },
   {
-    path: "/product-detail",
-    element: <ProductDetailPage></ProductDetailPage>,
+    path: "/product-detail/:id",
+    element: (
+      <Protected>
+        <ProductDetailPage></ProductDetailPage>
+      </Protected>
+    ),
   },
 ]);
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchItemsByUserIdAsync(user.id));
+    }
+  }, [dispatch, user]);
+
   return (
     <div className="App">
       <RouterProvider router={router} />
--- frontend/src/features/product/ProductAPI.js
+++ frontend/src/features/product/ProductAPI.js
@@ -7,67 +7,60 @@ export function fetchAllProducts() {
   });
 }
 
+export function fetchProductById(id) {
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch("http://localhost:8080/products/" + id);
+    
+    const data = await response.json();
+    resolve({ data });
+  });
+}
+
 export function fetchProductsByFilters(filter, sort, pagination) {
   // filter = {"category":["smartphone","laptops"]}
   // sort = {_sort:"price",_order="desc"}
   // pagination = {_page:1,_limit=10}
-
+  // TODO : on server we will support multi values in filter
   let queryString = "";
-
-  // Handle filters
   for (let key in filter) {
     const categoryValues = filter[key];
     if (categoryValues.length) {
-      for (let value of categoryValues) {
-        queryString += `${key}=${value}&`;
-      }
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
     }
   }
-
-  // Handle sorting
   for (let key in sort) {
     queryString += `${key}=${sort[key]}&`;
   }
-
-  // Handle pagination
+  console.log(pagination);
   for (let key in pagination) {
     queryString += `${key}=${pagination[key]}&`;
   }
 
-  // Remove the trailing '&'
-  queryString = queryString.slice(0, -1);
-
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await fetch(
-        "http://localhost:8080/products?" + queryString
-      );
-      const data = await response.json();
-
-      // Log headers for debugging
-      console.log([...response.headers.entries()]);
-
-      const totalItems = 100;
-      resolve({ data: { products: data, totalItems: +totalItems } });
-    } catch (error) {
-      reject(error);
-    }
+  return new Promise(async (resolve) => {
+    //TODO: we will not hard-code server URL here
+    const response = await fetch(
+      "http://localhost:8080/products?" + queryString
+    );
+    const data = await response.json();
+    const totalItems = await response.headers.get("X-Total-Count");
+    resolve({ data: { products: data, totalItems: +totalItems } });
   });
 }
 
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/categories");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
 
-// export function fetchCategories() {
-//   return new Promise(async (resolve) => {
-//     const response = await fetch("http://localhost:8080/categories");
-//     const data = await response.json();
-//     resolve({ data });
-//   });
-// }
-
-// export function fetchBrands() {
-//   return new Promise(async (resolve) => {
-//     const response = await fetch("http://localhost:8080/brands");
-//     const data = await response.json();
-//     resolve({ data });
-//   });
-// }
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/brands");
+    const data = await response.json();
+    resolve({ data });
+  });
+}
